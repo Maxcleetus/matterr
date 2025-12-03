4d28aecad43463c2b4d39a0c6ce958056d88e50c
max cleetus <maxcleetus@gmail.com>
2025-12-04 03:38:09 +0530

    allowed origin

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,8 @@ connectDB();
 // ✅ CORS configuration
 const allowedOrigins = [
   "http://localhost:5174",
-  "http://localhost:5173"
+  "http://localhost:5173",
+  "https://matterr.vercel.app/"
 ];
 
 app.use(cors({
