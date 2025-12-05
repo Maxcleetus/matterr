6e3f68b1b5c4a5c40bc863e233ced088da70af33
max cleetus <maxcleetus@gmail.com>
2025-12-05 16:58:23 +0530

    local host link changed

--- adminpanel/src/componenets/Dashboard.jsx
+++ adminpanel/src/componenets/Dashboard.jsx
@@ -91,7 +91,7 @@ const Dashboard = () => {
       if (!token) return navigate("/login");
 
       try {
-        const res = await fetch("http://localhost:5000/api/profile", {
+        const res = await fetch("https://jinto-backend.vercel.app/api/profile", {
           headers: { Authorization: `Bearer ${token}` },
         });
         if (!res.ok) throw new Error("Unauthorized");
